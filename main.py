369bc0afa8dfd602661139c84f7f3c1bb0461b10
vapvin <tpehdj12@daum.net>
2019-12-13 13:47:58 +0900

    Conditional Check

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ ns = "\n"
 cle = " "
 
 list = [[0]*4 for i in range(3)]
+list2 = [['?']*4 for q in range(3)]
 
 print(list)
 
@@ -14,6 +15,20 @@ for j in range(len(list)):
         list[j][k] = ans
 
 
-for a in range(len(list)):
-    for b in range(len(list[j])):
-        print(f"[{a}][{b}]: ?", end=f"{ns if (b==3 or b==6 or b==9) else cle}")
+for a in range(len(list2)):
+    for b in range(len(list2[j])):
+        print(f"[{a}][{b}]: {list2[a][b]}",
+              end=f"{ns if (b==3 or b==6 or b==9) else cle}")
+
+
+print("맞추실 행과 열을 입력해 주세요")
+
+ans1 = int(input("행 :"))
+ans2 = int(input("열 :"))
+
+print(f"[{ans1}][{ans2}] 열을 입력하였습니다.")
+print("정답을 입력해주세요")
+ans3 = int(input("답 :"))
+
+if ans3 == list[ans1][ans2]:
+    pass
