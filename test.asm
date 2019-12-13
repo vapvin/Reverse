9454ccc32940afad7bfd26fd5a059d9d85703d54
vapvin <tpehdj12@daum.net>
2019-12-13 13:25:41 +0900

    Test Print

---
+++ test.asm
@@ -0,0 +1,22 @@
+global start
+section .text
+
+start:
+        mov rax, 0x2000004
+        mov rdi, 1
+        mov rsi, msg
+        mov rdx, msg.len
+        syscall
+
+        mov rax, 0x2000001
+        mov rdi, 0
+        syscall
+
+section .data
+
+msg: db " Assembley To Interasting ", 10
+.len: equ $ - msg
+
+
+
+
